66df14f607711021ccec7a6f71d0ec67a004aefc
Marcos <39302713+marcosxmh@users.noreply.github.com>
2024-11-14 10:59:04 +0000

    Update App.java (Añade otro mensaje para prueba)

--- src/main/java/com/es/ull/app/App.java
+++ src/main/java/com/es/ull/app/App.java
@@ -6,5 +6,6 @@ package com.es.ull.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Esto es otro mensaje");
     }
 }
